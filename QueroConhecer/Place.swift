712974d773f602385175a68a3dfaf1de1bb31db2
mihcristina <contato@michellicristina.com.br>
2022-08-12 17:49:28 -0300

    feat: add methods of gesture

--- QueroConhecer/Place.swift
+++ QueroConhecer/Place.swift
@@ -17,4 +17,30 @@ struct Place {
     var coordinate: CLLocationCoordinate2D {
         return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
     }
+    
+    static func getFormattedAddress(with placemark: CLPlacemark) -> String {
+        var address = ""
+        if let street = placemark.thoroughfare {
+            address += street
+        }
+        if let number = placemark.subThoroughfare {
+            address += " \(number)"
+        }
+        if let subLocality = placemark.subLocality {
+            address += ", \(subLocality)"
+        }
+        if let city = placemark.locality {
+            address += "\n\(city)"
+        }
+        if let state = placemark.administrativeArea {
+            address += " - \(state)"
+        }
+        if let postalCode = placemark.postalCode {
+            address += "\nCEP: \(postalCode)"
+        }
+        if let country = placemark.country {
+            address += "\n\(country)"
+        }
+        return address
+    }
 }
--- QueroConhecer/PlaceFinderViewController.swift
+++ QueroConhecer/PlaceFinderViewController.swift
@@ -10,16 +10,43 @@ import MapKit
 
 class PlaceFinderViewController: UIViewController {
 
+    enum PlaceFinderMessageType {
+        case error(String)
+        case confirmation(String)
+    }
+    
     
     @IBOutlet weak var tfCity: UITextField!
     @IBOutlet weak var aiLoading: UIActivityIndicatorView!
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var viLoading: UIView!
     
+    var place: Place!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let gesture = UILongPressGestureRecognizer(target: self, action: #selector(getLocation(_:)))
+        gesture.minimumPressDuration = 2.0
+        mapView.addGestureRecognizer(gesture)
+    }
+    
+    @objc func getLocation(_ gesture: UITapGestureRecognizer) {
+        if gesture.state == .began {
+            load(show: true)
+            let point = gesture.location(in: mapView)
+            let coordinate = mapView.convert(point, toCoordinateFrom: mapView)
+            let location = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+            CLGeocoder().reverseGeocodeLocation(location) { placemarks, error in
+                self.load(show: false)
+                if error == nil {
+                    if !self.savePlace(with: placemarks?.first) {
+                        self.showMessage(type: .error("Não foi encontrado nenhum local com esse nome"))
+                    } else {
+                        self.showMessage(type: .error("Erro desconhecido"))
+                    }
+                }
+            }
+        }
     }
     
     @IBAction func findCity(_ sender: Any) {
@@ -29,7 +56,56 @@ class PlaceFinderViewController: UIViewController {
         let geoCoder = CLGeocoder()
         geoCoder.geocodeAddressString(address) { placemarks, error in
             self.load(show: false)
+            if error == nil {
+                if !self.savePlace(with: placemarks?.first) {
+                    self.showMessage(type: .error("Não foi encontrado nenhum local com esse nome"))
+                } else {
+                    self.showMessage(type: .error("Erro desconhecido"))
+                }
+            }
         }
+    }
+    
+    func savePlace(with placemark: CLPlacemark?) -> Bool {
+        guard let placemark = placemark, let coordinate = placemark.location?.coordinate else {
+            return false
+        }
+        let name = placemark.name ?? placemark.country ?? "Desconhecido"
+        let address = Place.getFormattedAddress(with: placemark)
+        place = Place(name: name, latitude: coordinate.latitude, longitude: coordinate.longitude, addreess: address)
+        
+        let region = MKCoordinateRegion(center: coordinate, span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+        mapView.setRegion(region, animated: true)
+        
+        self.showMessage(type: .confirmation(place.name))
+        
+        return true
+    }
+    
+    func showMessage(type: PlaceFinderMessageType) {
+        let title: String, message: String
+        var hasConfirmation: Bool = false
+        
+        switch type {
+            case .confirmation(let name):
+                title = "Local encontrado"
+                message = "Deseja adicionar \(name)?"
+                hasConfirmation = true
+            case .error(let errorMessage):
+                title = "Erro"
+                message = errorMessage
+        }
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        if hasConfirmation {
+            let confirmAction = UIAlertAction(title: "OK", style: .default) { action in
+                print("OK")
+            }
+            alert.addAction(confirmAction)
+        }
+        present(alert, animated: true, completion: nil)
     }
     
     func load(show: Bool) {
